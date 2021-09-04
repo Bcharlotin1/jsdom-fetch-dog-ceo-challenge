957492f39a3196904fedb7849688ee27bf5b5b50
bcharlotin1 <biancacharlotin1@gmail.com>
2021-09-04 17:23:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchImages(){
+    fetch(`${imgUrl}`)
+    .then(response => response.json())
+    .then(json => json);
+
+    
+}
+
+function addImages(){
+    const images = fetchImages();
+
+    element = document.getElementById("breed-dropdown");
+
+    for(const option of element){
+        for(const image of images){
+            option.innerHTML += `
+            <img src=${image}>
+            
+            `
+        }
+        
+    }
+}
